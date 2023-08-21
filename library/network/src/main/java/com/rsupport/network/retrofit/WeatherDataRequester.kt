4572116775650ec05a4afec8e89e50f2c539c3c0
lyh990517 <lyh990517@naver.com>
2023-08-21 09:25:25 +0900

    네트워크 에러 처리

--- library/network/src/main/java/com/rsupport/network/retrofit/WeatherDataRequester.kt
+++ library/network/src/main/java/com/rsupport/network/retrofit/WeatherDataRequester.kt
@@ -4,6 +4,7 @@ import com.rsupport.network.api.WeatherService
 import com.rsupport.network_contract.response.ApiResponse
 import com.rsupport.network_contract.IWeatherDataRequester
 import com.rsupport.network_contract.response.WeatherResponse
+import java.net.UnknownHostException
 import javax.inject.Inject
 
 class WeatherDataRequester @Inject constructor(
@@ -18,21 +19,25 @@ class WeatherDataRequester @Inject constructor(
         nx: Int,
         ny: Int
     ): ApiResponse<WeatherResponse?> {
-        val response = weatherService.getWeather(
-            serviceKey = "IzXw1F88L1rL6JJuOYmZ+8lCgnUTMhLSAjPaeS6yf2R58uvo1Bki9dBhl/y9VzA97RRD4ff4XTgLfuCtB7jwFw==",
-            pageNo,
-            numOfRows,
-            dataType,
-            baseDate,
-            baseTime,
-            nx,
-            ny
-        )
-        val apiResponse = if (response.isSuccessful) {
-            ApiResponse.Success(response.body())
-        } else {
-            ApiResponse.Fail(Throwable("Error!"))
+        try {
+            val response = weatherService.getWeather(
+                serviceKey = "IzXw1F88L1rL6JJuOYmZ+8lCgnUTMhLSAjPaeS6yf2R58uvo1Bki9dBhl/y9VzA97RRD4ff4XTgLfuCtB7jwFw==",
+                pageNo,
+                numOfRows,
+                dataType,
+                baseDate,
+                baseTime,
+                nx,
+                ny
+            )
+            val apiResponse = if (response.isSuccessful) {
+                ApiResponse.Success(response.body())
+            } else {
+                ApiResponse.Fail(Throwable("Load Fail"))
+            }
+            return apiResponse
+        }catch (e: UnknownHostException){
+            return ApiResponse.Fail(e.fillInStackTrace())
         }
-        return apiResponse
     }
 }
