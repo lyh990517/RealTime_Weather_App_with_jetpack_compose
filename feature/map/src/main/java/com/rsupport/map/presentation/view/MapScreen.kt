cba5ca8a93b106b6b660623379ed029fe1d416db
lyh990517 <lyh990517@naver.com>
2023-08-21 09:25:11 +0900

    state에 따른 화면처리

--- feature/map/src/main/java/com/rsupport/map/presentation/view/MapScreen.kt
+++ feature/map/src/main/java/com/rsupport/map/presentation/view/MapScreen.kt
@@ -4,24 +4,45 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.compose.ExperimentalNaverMapApi
 import com.naver.maps.map.compose.MapProperties
 import com.naver.maps.map.compose.MapUiSettings
+import com.naver.maps.map.compose.Marker
+import com.naver.maps.map.compose.MarkerState
 import com.naver.maps.map.compose.NaverMap
+import com.naver.maps.map.overlay.OverlayImage
+import com.rsupport.core.model.WeatherUiModel
+import com.rsupport.map.R
 import com.rsupport.map.presentation.state.MapState
+
+@Composable
+fun MapScreen(state: State<MapState>){
+    when(state.value) {
+        is MapState.Loading -> CircularProgressIndicator()
+        is MapState.Success -> MapContent((state.value as MapState.Success).data)
+        is MapState.Fail -> LoadingFail()
+    }
+}
+
+@Composable
+fun LoadingFail() {
+    Text(text = "LoadingFail")
+}
 
 @OptIn(ExperimentalNaverMapApi::class)
 @Composable
-fun MapScreen(state: State<MapState>){
+fun MapContent(weatherData: WeatherUiModel) {
     val mapProperties = remember {
         mutableStateOf(
-            MapProperties(maxZoom = 10.0, minZoom = 5.0)
+            MapProperties(maxZoom = 16.0, minZoom = 15.0)
         )
     }
     val mapUiSettings = remember {
@@ -30,7 +51,17 @@ fun MapScreen(state: State<MapState>){
         )
     }
     Box(Modifier.fillMaxSize()) {
-        NaverMap(properties = mapProperties.value, uiSettings = mapUiSettings.value)
+        NaverMap(properties = mapProperties.value, uiSettings = mapUiSettings.value){
+            Marker(
+                state = MarkerState(position = LatLng(37.532600, 127.024612)),
+                captionText = "Marker in Seoul",
+                icon = OverlayImage.fromResource(com.rsupport.ui_component.R.drawable.ic_launcher_background)
+            )
+            Marker(
+                state = MarkerState(position = LatLng(37.390791, 127.096306)),
+                captionText = "Marker in Pangyo"
+            )
+        }
         Column {
             Button(onClick = {
                 mapProperties.value = mapProperties.value.copy(
